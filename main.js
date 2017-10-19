79df6b8a99777e27270c469460f86e04e8676325
hogelog <konbu.komuro@gmail.com>
2017-10-19 23:22:58 +0900

    Show makecode.microbit.org

--- main.js
+++ main.js
@@ -15,15 +15,7 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.loadURL('https://makecode.microbit.org/')
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
